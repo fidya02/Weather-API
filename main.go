5cc6cc0b88a2121867ce00dc1d9ef9dfb7d6cc78
fidya <fidya@Fidyaa-MacBook-Air.local>
2023-11-11 02:49:45 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Location struct {
+	Name           string  `json:"name"`
+	Region         string  `json:"region"`
+	Country        string  `json:"country"`
+	Lat            float64 `json:"lat"`
+	Lon            float64 `json:"lon"`
+	LocaltimeEpoch int     `json:"localtime_epoch"`
+	Localtime      string  `json:"localtime"`
+}
+
+type Condition struct {
+	Text string `json:"text"`
+}
+
+type Current struct {
+	TempC     float64   `json:"temp_c"`
+	Humidity  int       `json:"humidity"`
+	Condition Condition `json:"condition"`
+	WindKph   float64   `json:"wind_kph"`
+}
+
+type WeatherResponse struct {
+	Location Location `json:"location"`
+	Current  Current  `json:"current"`
+}
+
+func fetchWeatherData(location string) (WeatherResponse, error) {
+	apiKey := "cc0a364fa16d48d0941135837230311"
+	url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=%s&q=%s", apiKey, location)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return WeatherResponse{}, err
+	}
+	defer response.Body.Close()
+
+	var weatherData WeatherResponse
+	err = json.NewDecoder(response.Body).Decode(&weatherData)
+	if err != nil {
+		return WeatherResponse{}, err
+	}
+
+	return weatherData, nil
+}
+
+func weatherHandler(c echo.Context) error {
+	// Dapatkan parameter lokasi dari query string
+	location := c.QueryParam("location")
+
+	// Lakukan pemeriksaan jika parameter lokasi kosong
+	if location == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Parameter Lokasi dibutuhkan"})
+	}
+
+	// Ambil data cuaca berdasarkan lokasi
+	weatherData, err := fetchWeatherData(location)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Gagal mengambil data cuaca"})
+	}
+
+	// Mengirimkan data cuaca sebagai respons JSON
+	return c.JSON(http.StatusOK, weatherData)
+}
+
+func main() {
+	e := echo.New()
+
+	// Serve HTML file at root ("/")
+	e.Static("/", "views") 
+
+	// Handle weather requests at the "/weather" endpoint
+	e.GET("/weather", weatherHandler)
+
+	// Start the server
+	port := 8080
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
+}
